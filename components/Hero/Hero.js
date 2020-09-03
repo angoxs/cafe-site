224c56b235aa27a5fe82fd162af26adc7ca83bfc
Angel <angelCL0@outlook.com>
2020-09-02 22:18:44 -0400

    Awesome

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import { styles } from "styled-system";
-
+import Fade from "react-reveal/Reveal";
 import style from "./hero.module.css";
+import Reveal from "react-reveal/Reveal";
 
 export default function Hero(props) {
   return (
@@ -8,12 +9,20 @@ export default function Hero(props) {
       <div className={style.hero_group}>
         <div className={style.title_container}>
           <span>INTRODUCING</span>
-          <h1>{props.title}</h1>
-          <p>
-            Learn to take advantage of other people’s work to further your own
-            cause.
-          </p>
-          <button className={style.btn}>LEARN MORE</button>
+          <Reveal effect="fadeInUp" effectOut="fadeOutLeft">
+            <h1>{props.title}</h1>
+          </Reveal>
+
+          <Fade up>
+            <p>
+              Learn to take advantage of other people’s work to further your own
+              cause.
+            </p>
+          </Fade>
+
+          <Fade up>
+            <button className={style.btn}>LEARN MORE</button>
+          </Fade>
         </div>
       </div>
     </div>
