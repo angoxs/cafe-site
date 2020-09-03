cf60e08cb4a3913e90cd5ec939e54242398b7909
Angel <angelCL0@outlook.com>
2020-09-02 23:27:09 -0400

    Nice

--- components/Showcase/Showcase.js
+++ components/Showcase/Showcase.js
@@ -1,34 +1,43 @@
 import style from "./show.module.css";
+import Fade from "react-reveal/Fade";
 
 export default function Showcase() {
   return (
     <div className={style.show_container}>
       <div className={style.show_grid}>
-        <div className={style.case}>
-          <div className={style.case_group}>
-            <p>SINGLE ORIGIN</p>
-            <h2>Ethipia Raro</h2>
-            <p>$19.00</p>
+        <Fade left>
+          <div className={style.case}>
+            <div className={style.case_group}>
+              <p>SINGLE ORIGIN</p>
+              <h2>Ethipia Raro</h2>
+              <p>$19.00</p>
+            </div>
           </div>
-        </div>
-        <div className={style.secondcase}>
-          <div className={style.case_group}>
-            <p>SINGLE ORIGIN</p>
-            <h2>Ethipia Raro</h2>
-            <p>$19.00</p>
+        </Fade>
+
+        <Fade bottom up>
+          <div className={style.secondcase}>
+            <div className={style.case_group}>
+              <p>SINGLE ORIGIN</p>
+              <h2>Ethipia Raro</h2>
+              <p>$19.00</p>
+            </div>
           </div>
-        </div>
-        <div className={style.thirdcase}>
-          <div className={style.thirdcase_group}>
-            <span>NEW ARRIVALS</span>
-            <h1>Freshest of the Fresh</h1>
-            <p>
-              Featuring the freshest in our beautiful range of seasonal coffees
-              sourced throughout the year.
-            </p>
-            <button>SHOP NOW</button>
+        </Fade>
+
+        <Fade right>
+          <div className={style.thirdcase}>
+            <div className={style.thirdcase_group}>
+              <span>NEW ARRIVALS</span>
+              <h1>Freshest of the Fresh</h1>
+              <p>
+                Featuring the freshest in our beautiful range of seasonal
+                coffees sourced throughout the year.
+              </p>
+              <button>SHOP NOW</button>
+            </div>
           </div>
-        </div>
+        </Fade>
       </div>
     </div>
   );
