17cc8582dfb2b6d98ab1cf4ab0e031086fffd865
Angel <angelCL0@outlook.com>
2020-09-03 13:45:10 -0400

    Clean

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,42 +1,41 @@
-import {
-  Container,
-  Grid,
-  FirstGroup,
-  SecondGroup,
-  ThirdGroup,
-  Text,
-  Button,
-} from "./style";
+import style from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <Container>
-      <Grid>
-        <Text>CAFINELI</Text>
-        <FirstGroup>
-          <Text>
+    <div className={style.footer_container}>
+      <div className={style.footer_grid}>
+        <p>CAFINELI</p>
+        <div className={style.footer_group}>
+          <p>
             <strong>Platform</strong>
-          </Text>
-          <Text>Help Center</Text>
-          <Text>Security</Text>
-        </FirstGroup>
-        <SecondGroup>
-          <Text>Customers</Text>
-          <Text>Use Cases</Text>
-          <Text>Customer Stories</Text>
-        </SecondGroup>
-        <ThirdGroup>
-          <Text>Company</Text>
-          <Text>About</Text>
-          <Text>
+          </p>
+
+          <p>Help Center</p>
+          <p>Security</p>
+        </div>
+        <div className={style.footer_group}>
+          <p>
+            <strong>Customers</strong>
+          </p>
+
+          <p>Use Cases</p>
+          <p>Customer Stories</p>
+        </div>
+        <div className={style.footer_group}>
+          <p>
+            <strong>Company</strong>
+          </p>
+
+          <p>About</p>
+          <p>
+            {" "}
             Careers <span>- We're hiring!</span>
-          </Text>
-        </ThirdGroup>
-        <Button>
-          {" "}
+          </p>
+        </div>
+        <button>
           <a href="https://twitter.com/angelVZUR">Apply</a>
-        </Button>
-      </Grid>
-    </Container>
+        </button>
+      </div>
+    </div>
   );
 }
